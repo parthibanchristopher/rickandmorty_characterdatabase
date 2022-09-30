4f2a42c8b7e00e15f001ef6ae4ef5b46d8f51ccf
parthibanchristopher <parthibanchristopher@gmail.com>
2022-09-30 09:46:13 +0800

    set up project

--- rickandmorty_characterdatabase/Views/HomeScreenView.swift
+++ rickandmorty_characterdatabase/Views/HomeScreenView.swift
@@ -7,15 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
+// MARK: Home Screen View
+struct HomeScreenView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
